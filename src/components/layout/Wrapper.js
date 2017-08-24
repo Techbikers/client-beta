4eed7528a358f0d2b2def055af7ec365fd3c7d16
Michael Willmott <mwillmott@gmail.com>
2017-08-24 16:56:38 +0100

    Refactor the Container & Wrapper components
    They are very similar so can be done more efficiently

--- src/components/layout/Wrapper.js
+++ src/components/layout/Wrapper.js
@@ -1,13 +1,9 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: ${props => props.column ? "column" : "row"};
-  justify-content: ${props => props.justify || "flex-start"};
-  align-items: ${props => props.align || "stretch"};
-  width: 100%;
-  max-width: 1120px;
+import Container from "components/layout/Container";
 
+const Wrapper = Container.extend`
+  max-width: 1120px;
   padding: 1rem;
   margin: 0 auto;
 `;
